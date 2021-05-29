eda9677a526ae0a81d1ee5f0ce2030b6d5144bf4
Pascal Eberlein <pascal@eberlein.io>
2021-05-29 13:39:49 +0200

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description=open("README.md", "r").read(),
     name="loraspi",
     version="1.5",
-    description="library for ",
+    description="library for lora stuff",
     author="Pascal Eberlein",
     author_email="pascal@eberlein.io",
     url="https://github.com/nbdy/loraspi",
@@ -15,18 +15,10 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License'
     ],
-    keywords="wardriving tool raspberry pi",
+    keywords="waveshare lora library",
     packages=find_packages(),
     install_requires=[
-        "scapy", "gpsd-py3", "loguru", "btpy", "spidev", "Pillow", "dataset", "pyrunnable", "pyclsload"
+        "pyrunnable", "loguru"
     ],
-    entry_points={
-        'console_scripts': [
-            'pwnpy = pwnpy.__main__:main'
-        ]
-    },
-    package_data={
-        "pwnpy": ["modules/*"]
-    },
     long_description_content_type="text/markdown",
 )
